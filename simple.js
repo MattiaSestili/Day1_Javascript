b4a80107280741a308bf20b6287ef50e329fa919
Mattia Sestili <mattia.sestili@gmail.com>
2016-06-13 15:37:29 +0100

    Sum of 2 numbers

--- simple.js
+++ simple.js
@@ -1,9 +1,13 @@
 //EVENT LISTENER
 var event1 = document.getElementById("button1");
 event1.addEventListener('click',myfirst,false);
+
 function myfirst(){
-//get user input 1
-var userinput1 = document.getElementById("input1").value;
-//write output 1 onto webpage
-document.getElementById("demo").innerHTML = userinput1;
+//get user input 1 convert in JS to a number
+var userinput1 = parseInt(document.getElementById("input1").value);
+//user input 2 convert in JS to a number    
+var userinput2 = parseInt(document.getElementById("input2").value);
+
+//output the sum
+document.getElementById("demo").innerHTML = userinput1 + userinput2;
 }
