b362719203353f222f737026d81d0990baa537ac
Mattia Sestili <mattia.sestili@gmail.com>
2016-06-13 16:15:39 +0100

    calculation

--- simple.js
+++ simple.js
@@ -1,6 +1,6 @@
 //EVENT LISTENER
-var event1 = document.getElementById("button1");
-event1.addEventListener('click',myfirst,false);
+//var event1 = document.getElementById("button1");
+//event1.addEventListener('click',myfirst,false);
 
 function myfirst(){
 //get user input 1 convert in JS to a number
@@ -16,4 +16,18 @@ if(userinput1 > userinput2){
     document.getElementById("demo").innerHTML = userinput2;
     console.log("The larger is " + userinput2);
 }
+}
+
+var event2 = document.getElementById("button1");
+event2.addEventListener('click',calculation,false);
+
+function calculation(){
+    var userinput1 = parseInt(document.getElementById("input1").value); 
+    var userinput2 = parseInt(document.getElementById("input2").value);  
+    var userinput3 = parseInt(document.getElementById("input3").value)
+    
+    document.getElementById("demo").innerHTML = (userinput1+userinput2)*userinput3;
+    
+    document.getElementById("demo2").innerHTML = userinput1 + userinput2 + userinput3;
+    
 }
